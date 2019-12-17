7e0d53a24f8632ff68adc4127c07273a85d086cf
Code Institute Production <matt.rudge@codeinstitute.net>
2019-12-17 14:11:15 +0000

    Updated support views to pass success message to student_care template

--- lms/djangoapps/ci_support/views.py
+++ lms/djangoapps/ci_support/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.db import transaction
 from django.views.decorators.cache import cache_control
 from django.conf import settings
+from django.contrib import messages
 from django.core.context_processors import csrf
 from django.core.mail import send_mail
 from util.views import ensure_valid_course_key
@@ -122,6 +123,9 @@ def studentcare(request, course_id, student_id=None):
             'email_subject': email_subject,
             'email_body': email_body
         })
+
+        messages.success(request, 'Thank you! Your message has been sent to our Student Care team. We\'ll respond to you shortly.')
+        
 
     return render_to_response(
         "courseware/support/student_care.html",
