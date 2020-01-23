a4bdc0d47c7498e1d684d856851bffa19b5ed4ec
Stefan Dworschak <stefan@codeinstitute.net>
2020-01-23 11:45:52 +0000

    making test changes

--- lms/djangoapps/learning_success/management/commands/export_all_activity_records.py
+++ lms/djangoapps/learning_success/management/commands/export_all_activity_records.py
@@ -140,8 +140,8 @@ def fractions_per_module(fraction_record, completed_fractions, days_ago=14):
     for module, fraction in completed_fractions.items():
         accessor = format_module_field(
                 module[0], '_fraction_within_%sd' % (days_ago)) 
-            if fraction['time_completed'] > n_days_ago 
-            else format_module_field(
+                if fraction['time_completed'] > n_days_ago 
+                else format_module_field(
                 module[0], '_fraction_before_%sd' % (days_ago))
         if accessor in fractions:
             fractions[accessor] += fraction['lesson_fraction']
