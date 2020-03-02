a78bd6572cccebba2f19cfb1a8c040affe4be31c
Stefan Dworschak <stefan@codeinstitute.net>
2020-03-02 12:35:29 +0000

    testing adding enrollments back in

--- lms/djangoapps/ci_program/views.py
+++ lms/djangoapps/ci_program/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from edxmako.shortcuts import render_to_response
 from ci_program.models import Program
+import logging
 
+logger = logging.getLogger(__name__)
 
 @login_required
 def show_programs(request, program_name):
@@ -10,10 +12,9 @@ def show_programs(request, program_name):
     """
     program = Program.objects.get(marketing_slug=program_name)
     program_descriptor = program.get_program_descriptor(request.user)
-    student_enrollment = [
-        enrollment.course_id 
-        for enrollment in request.user.courseenrollment_set.all()]
-
+    student_enrollment = request.user.courseenrollment_set.all()
+    logger.error('Student Enrollment')
+    logger.error(student_enrollment)
     context = {}
     context["program"] = program_descriptor
     context["student_enrollement"] = student_enrollment
