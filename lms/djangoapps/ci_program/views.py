32ff23b8476f96a07b0f05916919b185858f5147
Stefan Dworschak <stefan@codeinstitute.net>
2020-03-03 15:46:10 +0000

    Refactor to query ORM directly

--- lms/djangoapps/ci_program/views.py
+++ lms/djangoapps/ci_program/views.py
@@ -10,14 +10,18 @@ def show_programs(request, program_name):
     """
     program = Program.objects.get(marketing_slug=program_name)
     program_descriptor = program.get_program_descriptor(request.user)
-    # Any data passed to context need to be dict
-    enrolled_course_codes = {}
-    for enrollment in request.user.courseenrollment_set.all():
-        is_active = enrollment._get_enrollment_state(
-            request.user, enrollment.course_id)[1]
-        enrolled_course_codes[enrollment.course_id] = is_active
-
-    context = {}
-    context["program"] = program_descriptor
-    context["enrolled_course_codes"] = enrolled_course_codes
+    enrolled_courses = request.user.courseenrollment_set.filter(is_active=True)
+    enrolled_keys = set(enrolled_courses.values_list('course_id', flat=True))
+    context = {'program': {
+        'full_description': program_descriptor['full_description'],
+        'length': program_descriptor['length'],
+        'subtitle': program_descriptor['subtitle'],
+        'name': program_descriptor['name'],  
+        'number_of_modules': len(enrolled_keys),
+        'video': program_descriptor['video'], 
+        'image': program_descriptor['image'], 
+        'effort': program_descriptor['effort'],
+        'modules': [m for m in program_descriptor['modules']
+                    if m['course_key'].html_id() in enrolled_keys],
+    }}
     return render_to_response('programs/programs.html', context)
