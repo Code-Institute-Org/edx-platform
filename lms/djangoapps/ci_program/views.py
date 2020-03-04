e96c50c858fb52de114d5d97a3a2ca4e3aa36e39
stefan-ci <58422733+stefan-ci@users.noreply.github.com>
2020-03-04 09:18:10 +0000

    Update the programs view to only show enrolled and active courses (#13)
    Retrieve the enrolled modules from the request.user object and check the program descriptor (which has all of the program's module) against it to create a filtered collection of only the modules the student is enrolled in and status active.

--- lms/djangoapps/ci_program/views.py
+++ lms/djangoapps/ci_program/views.py
@@ -10,7 +10,18 @@ def show_programs(request, program_name):
     """
     program = Program.objects.get(marketing_slug=program_name)
     program_descriptor = program.get_program_descriptor(request.user)
-
-    context = {}
-    context["program"] = program_descriptor
+    enrolled_courses = request.user.courseenrollment_set.filter(is_active=True)
+    enrolled_keys = set(enrolled_courses.values_list('course_id', flat=True))
+    context = {'program': {
+        'full_description': program_descriptor['full_description'],
+        'length': program_descriptor['length'],
+        'subtitle': program_descriptor['subtitle'],
+        'name': program_descriptor['name'],  
+        'number_of_modules': len(enrolled_keys),
+        'video': program_descriptor['video'], 
+        'image': program_descriptor['image'], 
+        'effort': program_descriptor['effort'],
+        'modules': [m for m in program_descriptor['modules']
+                    if m['course_key'].html_id() in enrolled_keys],
+    }}
     return render_to_response('programs/programs.html', context)
