66e7ae71e79d9651b41a64be8d24822450ea9b1d
Stefan Dworschak <stefan@codeinstitute.net>
2020-03-11 16:23:49 +0000

    Adding speed adjustment fix from edx/edx-platform
    Main change was the change from string to float speed
    variable format and some conversions
    
    See Source here:
    https://github.com/edx/edx-platform/commit/a5fe66863eef403ffa4606a22c81ec2a367f1986
    https://github.com/edx/edx-platform/pull/19708/files

--- common/lib/xmodule/xmodule/js/src/video/03_video_player.js
+++ common/lib/xmodule/xmodule/js/src/video/03_video_player.js
@@ -422,7 +422,7 @@ function(HTML5Video, HTML5HLSVideo, Resizer, HLS, _) {
             );
         }
 
-        newSpeed = parseFloat(newSpeed).toFixed(2).replace(/\.00$/, '.0');
+        newSpeed = parseFloat(newSpeed).toFixed(2);
         this.setSpeed(newSpeed);
         this.videoPlayer.setPlaybackRate(newSpeed);
     }
