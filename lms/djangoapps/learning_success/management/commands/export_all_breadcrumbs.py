52760ad40cf41245fc60e64011c995a8a34a70b7
stefan-ci <58422733+stefan-ci@users.noreply.github.com>
2020-03-18 08:21:07 +0000

    Updated breadcrumbs logic (#12)
    Changed harvest_course_tree to work with a list of dict as output instead of a massive dict which enables easier work with pandas.
    
    Created a logic whereby module, section, lesson, unit and component are only populated if applicable (e.g. if the block is a module, the rest of the fields will be None)
    
    Created function like dict.get() which safely extracts an index from a list and returns None if the index is out of bounds.

--- lms/djangoapps/learning_success/management/commands/export_all_breadcrumbs.py
+++ lms/djangoapps/learning_success/management/commands/export_all_breadcrumbs.py
@@ -13,14 +13,13 @@ import json
 
 import requests
 
+LMS_TABLE = 'lms_breadcrumbs_v3'
+
 # Need to create an engine using sqlalchemy to be able to
 # connect with pandas .to_sql
 # Pandas natively only supports sqlite3
 # '?charset=utf8' used to specify utf-8 encoding to avoid encoding errors
-
-LMS_TABLE = 'lms_breadcrumbs_v3'
-
-CONNECTION_STRING = 'mysql+mysqldb://%s:%s@%s:%d/%s%s' % (
+CONNECTION_STRING = 'mysql+mysqldb://%s:%s@%s:%s/%s%s' % (
     settings.RDS_DB_USER,
     settings.RDS_DB_PASS,
     settings.RDS_DB_ENDPOINT,
@@ -30,34 +29,61 @@ CONNECTION_STRING = 'mysql+mysqldb://%s:%s@%s:%d/%s%s' % (
 
 BREADCRUMB_INDEX_URL = settings.LMS_SYLLABUS
 
+# Keys are representing the internal edx terminology
+# and the values are our CI terminology (also based on edx)
+BLOCK_TYPES = {
+    'course': 'module',
+    'chapter': 'section',
+    'sequential': 'lesson',
+    'vertical': 'unit',
+}
+
 PROGRAM_CODE = 'FS'  # Our Full-Stack program
 
-def harvest_course_tree(tree, output_dict, prefix=()):
+
+def get_safely(breadcrumbs, index):
+    try:
+        return breadcrumbs[index]
+    except IndexError as e:
+        return None
+
+
+def harvest_course_tree(tree, output_list, prefix=()):
     """Recursively harvest the breadcrumbs for each component in a tree
 
     Populates output_dict
     """
-    block_name = tree.display_name
     block_breadcrumbs = prefix + (tree.display_name,)
-    block_id = tree.location.block_id
 
-    output_dict[block_id] = block_breadcrumbs
+    block = {}
+    block['block_id'] = tree.location.block_id
+    block['block_name'] = tree.display_name
+    block['block_type'] = BLOCK_TYPES.get(tree.category) or 'component'
+    block['module'] = get_safely(block_breadcrumbs, 0)
+    block['section'] = get_safely(block_breadcrumbs, 1)
+    block['lesson'] = get_safely(block_breadcrumbs, 2)
+    block['unit'] = get_safely(block_breadcrumbs, 3)
+    block['breadcrumbs'] = ' - '.join(block_breadcrumbs)
+    block['lms_category'] = tree.category
+
+    output_list.append(block)
 
     children = tree.get_children()
     for subtree in children:
-        harvest_course_tree(subtree, output_dict, prefix=block_breadcrumbs)
-
+        harvest_course_tree(subtree, output_list, prefix=block_breadcrumbs)
+    
 
 def harvest_program(program):
     """Harvest the breadcrumbs from all components in the program
 
-    Returns a dictionary mapping block IDs to the matching breadcrumbs
+    Returns a list of dictionaries containing xblock meta data
     """
-    all_blocks = {}
+    all_blocks = []
     for course_locator in program.get_course_locators():
         course = modulestore().get_course(course_locator)
         harvest_course_tree(course, all_blocks)
     return all_blocks
+
 
 def get_breadcrumb_index(URL):
     """Retrieve the course syllabus from Google Sheet with the ordering
@@ -69,11 +95,11 @@ def get_breadcrumb_index(URL):
     df_breadcrumb_idx = df_breadcrumb_idx.T
     df_breadcrumb_idx = df_breadcrumb_idx.reset_index()
     df_breadcrumb_idx.rename(columns={'index':'order_index'}, inplace=True)
-
     # TODO: remove following line, once the [beta] suffix is removed in the LMS
-    df_breadcrumb_idx['module'] = df_breadcrumb_idx['module'].replace('Careers','Careers [Beta]')
+    df_breadcrumb_idx['module'] = df_breadcrumb_idx['module'].replace(
+        'Careers', 'Careers [Beta]')
     df_breadcrumb_idx = df_breadcrumb_idx.reset_index()
-    return df_breadcrumb_idx[['module','lesson','order_index']]
+    return df_breadcrumb_idx[['module','lesson','order_index','time_fraction']]
 
 
 class Command(BaseCommand):
@@ -82,23 +108,16 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         program = get_program_by_program_code(PROGRAM_CODE)
-        all_components = harvest_program(program)
-        
-        # Looping through the results because you cannot convert 
-        # Variable length dict list to DataFrame, but list of lists
-        components = [[k] + v for k, v in d.iteritems()]
-        df = pd.DataFrame(components)
-        # Need to assign headers
-        df.columns = ['uuid','module','section','lesson','unit','type']
+        all_components = harvest_program(program)        
+        df = pd.DataFrame(all_components)
         
         # Need to get lesson order from syllabus for ordering the modules
-        # And not going back
+        # And course fractions
         df_breadcrumb_idx = get_breadcrumb_index(BREADCRUMB_INDEX_URL)
         df = df.merge(df_breadcrumb_idx, on=['module', 'lesson'], how='left')
         
         engine = create_engine(CONNECTION_STRING, echo=False)
-        df.to_sql(name=LMS_TABLE, con=engine, if_exists='replace',
-                    dtype={'uuid': types.VARCHAR(length=255),
-                            'order_index': types.INT
-                    })
-
+        df.to_sql(name=LMS_TABLE, 
+                  con=engine, 
+                  if_exists='replace', 
+                  dtype={'order_index': types.INT})
