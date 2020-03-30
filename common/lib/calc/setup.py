ed1573f1b95d7f57de4d0eb0f21a42e30ecf2c4c
Stefan Dworschak <stefan@codeinstitute.net>
2020-03-30 14:38:11 +0100

    Changing the numpy version for the right pandas installation on python2

--- common/lib/calc/setup.py
+++ common/lib/calc/setup.py
@@ -6,7 +6,7 @@ setup(
     packages=["calc"],
     install_requires=[
         "pyparsing==2.0.7",
-        "numpy==1.18.2",
+        "numpy==1.16.6",
         "scipy==0.14.0",
     ],
 )
--- common/lib/chem/setup.py
+++ common/lib/chem/setup.py
@@ -6,7 +6,7 @@ setup(
     packages=["chem"],
     install_requires=[
         "pyparsing==2.0.7",
-        "numpy==1.18.2",
+        "numpy==1.16.6",
         "scipy==0.14.0",
         "nltk==2.0.6",
     ],
