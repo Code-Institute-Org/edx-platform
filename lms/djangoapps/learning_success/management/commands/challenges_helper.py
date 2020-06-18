94e0e19e665ac54b9cb7c142175d63c78b9f807f
stefdworschak <stef.dworschak@gmail.com>
2020-06-18 15:04:49 +0100

    Adding challenge_skills calculation

--- lms/djangoapps/learning_success/management/commands/challenges_helper.py
+++ lms/djangoapps/learning_success/management/commands/challenges_helper.py
@@ -3,6 +3,24 @@ from challenges.models import Challenge
 from collections import Counter, defaultdict
 import json
 
+from lms.djangoapps.learning_success.management.commands.export_all_breadcrumbs import get_safely
+
+DEFAULT_SKILL = {
+    "achieved": 0,
+    "total": 0,
+}
+
+
+def increment_student_skill_tags(challenge_skills, skill_tags, incrementor):
+    """ Increments the student's achieved skill tags based on the skill tags
+    of the challenge
+    
+    Modifies dict inplace """ 
+    for skill in skill_tags:
+        challenge_skills.set_default(skill, DEFAULT_SKILL)
+        challenge_skills[skill]['achieved'] += incrementor
+        challenge_skills[skill]['total'] += 1
+
 
 def index_challenge_to_module_and_level():
     """ Collects all Challeges in the LMS
@@ -11,10 +29,14 @@ def index_challenge_to_module_and_level():
 
     Returns a dict of all challenges with its PK and category"""
     challenge_index = {}
+    skill_tags = {}
     for challenge in Challenge.objects.all():
+        update_skills_tags(skill_tags, challenge)
         module = challenge.block_locator.split('+')[1].lower()
         module_level = "_".join((module, challenge.level)).lower()
-        challenge_index[challenge.pk] = module_level
+        challenge_index[challenge.pk] = {
+            'module_name': module_level,
+            'tags': [tag.name for tag in challenge.tags.all()]
     return challenge_index
 
 
@@ -25,13 +47,18 @@ def single_student_challenge_history(student, challenge_counter,
     Returns a dict with with passed, attempted and unattempted counts """
     challenge_activities = {module: defaultdict(int) for module
                             in challenge_counter.keys()}
-
+    student_skills = {}
     for submission in student.challengesubmission_set.all():
-        module = challenge_index[submission.challenge_id]
+        module = challenge_index[submission.challenge_id].get('module_name')
+        skill_tags = challenge_index[submission.challenge_id].get('tags')
+        incrementor = 0
         if submission.passed:
             challenge_activities[module]['passed'] += 1
+            incrementor = 1
         else:
             challenge_activities[module]['attempted'] += 1
+        increment_student_skill_tags(increment_student_skill_tags,
+                                     skill_tags, incrementor)
 
     for module_level, total_challenges in challenge_counter.items():
         activities = challenge_activities[module_level]
@@ -42,7 +69,7 @@ def single_student_challenge_history(student, challenge_counter,
         module: json.dumps(challenges)
         for module, challenges in challenge_activities.items()
     }
-
+    challenge_activities['student_skills'] = skill_tags
     return challenge_activities
 
 
