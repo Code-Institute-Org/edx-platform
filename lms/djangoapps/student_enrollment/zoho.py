1ee3df24bfd2d4a434f1686e77d5171ef7c5577b
nakitamccool <nakitamccool@gmail.com>
2020-06-22 17:53:31 +0100

    Remove print statement, previously used to debug CRM API issue

--- lms/djangoapps/student_enrollment/zoho.py
+++ lms/djangoapps/student_enrollment/zoho.py
@@ -75,7 +75,6 @@ def get_students_to_be_enrolled_in_careers_module():
 
         students.extend(students_resp.json()['data'])
         if not students_resp.json()['info']['more_records']:
-            print(students)
             return students
 
 
