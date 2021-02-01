09af41647d39818c5c5e9ee44b002dcf798fc274
Nakita <nakitamccool@gmail.com>
2021-02-01 10:37:52 +0000

    Add management command to unenroll all enrolled students from a specific program

---
+++ lms/djangoapps/student_enrollment/management/commands/unenroll_all_enrolled_students_from_program.py
@@ -0,0 +1,17 @@
+from ci_program.models import Program
+
+def unenroll_all_enrolled_students(program_code):
+    program = Program.objects.get(program_code=program_code)
+    enrolled_students = program.enrolled_students.all()
+    for student in enrolled_students:
+        program.unenroll_student_from_program(student)
+
+
+class Command(BaseCommand):
+    help = 'Unenroll all enrolled students from a specific program'
+
+    def add_arguments(self, parser):
+        parser.add_argument('program_code', type=str)
+
+    def handle(self, program_code, **kwargs):
+        unenroll_all_enrolled_students(program_code)
